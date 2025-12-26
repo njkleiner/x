e2a7ae04601f3e4c9359f12478f77232972adf97
Noah Kleiner <noah@njkleiner.com>
2025-12-26 12:44:06 +0100

    log: expand unit test coverage
    ... test all level-based log functions and also cover the case where
    the message is discarded because it does not meet the minimum level.

--- log/log_test.go
+++ log/log_test.go
@@ -85,3 +85,79 @@ func TestWith(t *testing.T) {
 		t.Errorf("invalid message: got=%q want=%q", got, want)
 	}
 }
+
+func TestHandle(t *testing.T) {
+	timeNow = func() time.Time {
+		// NOTE: override the "current" timestamp with a static value during testing
+		// to be able to compare the actual against the expected log message output.
+		return time.Date(2025, time.December, 25, 12, 28, 19, 0, time.UTC)
+	}
+
+	t.Cleanup(func() { timeNow = time.Now })
+
+	tests := []struct {
+		name  string
+		print func(ctx context.Context, msg string, args ...any)
+
+		options *slog.HandlerOptions
+
+		want string
+	}{
+		{
+			name:  "discard",
+			print: Debug,
+
+			options: &slog.HandlerOptions{Level: slog.LevelInfo},
+
+			want: ``,
+		},
+		{
+			name:  "debug",
+			print: Debug,
+
+			options: &slog.HandlerOptions{Level: slog.LevelDebug},
+
+			want: `{"time":"2025-12-25T12:28:19Z","level":"DEBUG","msg":"hello world","answer":42}`,
+		},
+		{
+			name:  "info",
+			print: Info,
+
+			options: &slog.HandlerOptions{Level: slog.LevelDebug},
+
+			want: `{"time":"2025-12-25T12:28:19Z","level":"INFO","msg":"hello world","answer":42}`,
+		},
+		{
+			name:  "warn",
+			print: Warn,
+
+			options: &slog.HandlerOptions{Level: slog.LevelDebug},
+
+			want: `{"time":"2025-12-25T12:28:19Z","level":"WARN","msg":"hello world","answer":42}`,
+		},
+		{
+			name:  "error",
+			print: Error,
+
+			options: &slog.HandlerOptions{Level: slog.LevelDebug},
+
+			want: `{"time":"2025-12-25T12:28:19Z","level":"ERROR","msg":"hello world","answer":42}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var buf bytes.Buffer
+
+			slog.SetDefault(slog.New(slog.NewJSONHandler(&buf, tt.options)))
+
+			ctx := context.Background()
+
+			tt.print(ctx, "hello world", slog.Int("answer", 42))
+
+			if got := strings.TrimSpace(buf.String()); got != tt.want {
+				t.Errorf("invalid message: got=%q want=%q", got, tt.want)
+			}
+		})
+	}
+}
